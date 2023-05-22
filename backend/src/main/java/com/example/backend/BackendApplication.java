d1d66acbebfc32b4a7a580ad8145e9ec56c1e23b
ad-world <67097683+ad-world@users.noreply.github.com>
2023-05-22 16:26:46 -0400

    Remove unnecessary sql statements, change selection statement.

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -18,8 +18,8 @@ public class BackendApplication {
 
     @GetMapping("/hello")
     public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
-        String username = "PUT_USERNAME_HERE"; // our default username is root
-        String password = "PUT_PASSWORD_HERE"; // our default password is password
+        String username = "root"; // our default username is root
+        String password = "password"; // our default password is password
         String dbName = "test_db"; // our current db name is test_db
 
         String url = "jdbc:mysql://127.0.0.1:3306/" + dbName;
@@ -27,47 +27,21 @@ public class BackendApplication {
 
         try {
             connection = DriverManager.getConnection(url, username, password);
-            System.out.println(connection);
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        String createStatement = "CREATE TABLE test_table (message varchar(255))";
-        try {
-            Statement statement = connection.createStatement();
-            boolean resultSet = statement.execute(createStatement);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        String addMessageQuery = "INSERT INTO test_table VALUES (\"Hello World\")";
-        try {
-            Statement statement = connection.createStatement();
-            boolean resultSet = statement.execute(addMessageQuery);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        String readMessageQuery = "SELECT message FROM test_table";
+        String readMessageQuery = "SELECT * FROM test_table";
         try {
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(readMessageQuery);
             resultSet.next();
-            name = resultSet.getString(1);
+            name = resultSet.getString(2);
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        String dropTableQuery = "DROP TABLE test_table";
-
-        try {
-            Statement statement = connection.createStatement();
-            boolean resultSet = statement.execute(dropTableQuery);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        // Going to http://localhost:8080/hello should return "Hello Hello World" since name = "Hello World" (from the DB).
+        // Going to http://localhost:8080/hello should return "Hello HelloWorld" since name = "Hello World" (from the DB).
         return String.format("Hello %s!", name);
     }
 }
