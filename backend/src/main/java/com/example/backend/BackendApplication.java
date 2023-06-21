133c91c44ceca9c0b4c5d93edca038e8c0658c4c
Justin Toft <justintoft10@gmail.com>
2023-06-21 16:07:38 -0400

    Revert "Added enpoint for playercount, improved how data is returned to frontend"
    This reverts commit 311beec739155438fe0866164dd18070c1695d49.

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,9 +1,7 @@
 package com.example.backend;
 
-import org.jooq.Record;
 import org.jooq.RecordMapper;
 import org.jooq.impl.DSL;
-import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,8 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.*;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 @SpringBootApplication
 @RestController
@@ -30,37 +26,6 @@ public class BackendApplication {
         SpringApplication.run(BackendApplication.class, args);
     }
 
-    // Source https://github.com/eugenp/tutorials/blob/master/persistence-modules/core-java-persistence-2/src/main/java/com/baeldung/resultset2json/ResultSet2JSON.java
-    // Released under MIT License
-    private static JSONArray resultToJsonArray(ResultSet resultSet, Connection dbConnection) throws SQLException {
-        ResultSetMetaData md = resultSet.getMetaData();
-        int numCols = md.getColumnCount();
-        List<String> colNames = IntStream.range(0, numCols)
-                .mapToObj(i -> {
-                    try {
-                        return md.getColumnName(i + 1);
-                    } catch (SQLException e) {
-
-                        e.printStackTrace();
-                        return "?";
-                    }
-                })
-                .toList();
-
-        List<JSONObject> json = DSL.using(dbConnection)
-                .fetch(resultSet)
-                .map(new RecordMapper<Record, JSONObject>() {
-
-                    @Override
-                    public JSONObject map(Record r) {
-                        JSONObject obj = new JSONObject();
-                        colNames.forEach(cn -> obj.put(cn, r.get(cn)));
-                        return obj;
-                    }
-                });
-        return new JSONArray(json);
-    }
-
     @GetMapping("/players")
     public String getPlayers(@RequestParam(name = "startId", defaultValue = "0") Integer startId,
                              @RequestParam(name = "endId", defaultValue = "50") Integer endId) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
@@ -70,30 +35,17 @@ public class BackendApplication {
                     + startId + " AND player_id < " + endId;
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(readMessageQuery);
-            JSONArray res = resultToJsonArray(resultSet, connection);
+            JSONObject result = new JSONObject(DSL.using(connection)
+                    .fetch(resultSet)
+                    .formatJSON());
 
-            return res.toString();
+            return result.toString();
         } catch (SQLException e) {
             e.printStackTrace();
             return String.format("Unable to parse JSON: %s", e);
         }
     }
 
-    @GetMapping("/playerCount")
-    public String getPlayerCount() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
-        try {
-            Connection connection = DriverManager.getConnection(url, username, password);
-            String readMessageQuery = "SELECT COUNT(*) AS playerCount FROM Players";
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(readMessageQuery);
-            JSONArray res = resultToJsonArray(resultSet, connection);
-
-            return res.toString();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return String.format("Unable to parse JSON: %s", e);
-        }
-    }
 
     @GetMapping("/player/{ID}")
     public String getPlayerByID(@PathVariable String ID) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
@@ -102,13 +54,16 @@ public class BackendApplication {
             String readMessageQuery = "SELECT * FROM Players WHERE player_id = " + ID;
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(readMessageQuery);
-            JSONArray res = resultToJsonArray(resultSet, connection);
+            JSONObject result = new JSONObject(DSL.using(connection)
+                    .fetch(resultSet)
+                    .formatJSON());
 
-            return res.toString();
+            return result.toString();
         } catch (SQLException e) {
             e.printStackTrace();
             return String.format("Unable to parse JSON: %s", e);
         }
     }
-    
+
+
 }
