b3abc341235aaee019756fe78cdc2f1ea7faa853
justun <j255gao@uwaterloo.ca>
2023-07-09 14:16:07 -0400

    wrong_predict_winner

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -79,4 +79,31 @@ public class BackendApplication {
             return ResponseEntity.badRequest().body(String.format("Unable to parse JSON: %s", e));
         }
     }
+
+    @GetMapping("/predictWinner/{user_id}/{teamName1}/{teamName2}")
+    public ResponseEntity<String> predictWinner(@PathVariable String user_id, String teamName1, String teamName2) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            String getTeam1Score = "With team_player_ids AS (SELECT player_id from isInUserTeam where user_team_id = (Select user_team_id from UserTeams natural join isOwnerOf where team_name = '" + teamName1 + "' and user_id = "+ user_id +"))" +
+                    "(SELECT (sum(finishing) + sum(dribbling) + sum(sprint_speed) + sum(strength) + sum(aggression) + sum(interceptions))/COUNT(*) AS team_score FROM team_player_ids natural join Players);";
+            String getTeam2Score = "With team_player_ids AS (SELECT player_id from isInUserTeam where user_team_id = (Select user_team_id from UserTeams natural join isOwnerOf where team_name = '" + teamName2 + "' and user_id = "+ user_id +"))" +
+                    "(SELECT (sum(finishing) + sum(dribbling) + sum(sprint_speed) + sum(strength) + sum(aggression) + sum(interceptions))/COUNT(*) AS team_score FROM team_player_ids natural join Players);";
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet1 = statement.executeQuery(getTeam1Score);
+            ResultSet resultSet2 = statement.executeQuery(getTeam2Score);
+            JSONArray res1 = util.resultToJsonArray(resultSet1, connection);
+            JSONArray res2 = util.resultToJsonArray(resultSet2, connection);
+            JSONObject object = new JSONObject();
+
+            statement.close();
+            System.out.println(res1.get(0));
+            return ResponseEntity.ok(res1.toString());
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body(String.format("Unable to parse JSON: %s", e));
+        }
+    }
+
+
 }
