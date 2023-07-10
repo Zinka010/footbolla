eae273afb786bbf0629fe48b878f2b2b1981f276
Justin Toft <justintoft10@gmail.com>
2023-07-09 21:49:43 -0400

    Revert "Added sample SQL for R10, update SQL for updated R6"
    This reverts commit 36b5ec166b22fcb48ae1e90c9fb5823a0dccb6ae.

--- sample-sql/feature-10a-sample-input.sql
+++
@@ -1,4 +0,0 @@
-SELECT P.name, PTH.season, P.player_id 
-FROM footyfiend.Teams as T, footyfiend.PlayerTeamHistory AS PTH, footyfiend.Players AS P 
-WHERE PTH.team_id = T.team_id AND P.player_id = PTH.player_id  AND PTH.team_id = 12
-ORDER BY PTH.season, P.name;
