871e6549871a42e584fb19dafeb3f40516d06a07
ad-world <67097683+ad-world@users.noreply.github.com>
2023-07-11 10:39:22 -0400

    R9 work for Milestone 2

---
+++ migration/relation-schema/indexes/USER_TEAMS.sql
@@ -0,0 +1,2 @@
+CREATE INDEX user_team_player_idx ON IsInUserTeam (player_id);
+CREATE INDEX user_team_id_idx ON IsInUserTeam (user_team_id);
---
+++ sample-sql/feature-9-sample-input-1.sql
@@ -0,0 +1,6 @@
+INSERT IGNORE INTO footyfiend.IsInUserTeam VALUES
+   (1, 1, 0),
+   (2, 1, 1),
+   (5, 2, 2),
+   (10, 1, 3),
+   (3, 2, 4);
