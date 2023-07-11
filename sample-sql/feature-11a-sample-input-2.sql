39606b422025eafc3d16865714a631eb26551d14
Justin Toft <justintoft10@gmail.com>
2023-07-11 12:49:37 -0400

    Minor formatting change

--- sample-sql/feature-11a-sample-input-2.sql
+++ sample-sql/feature-11a-sample-input-2.sql
@@ -2,5 +2,5 @@ SELECT *
 FROM footyfiend.MatchesPlayed NATURAL JOIN footyfiend.Matches 
 WHERE (home_team_id = 12 AND away_team_id = 14) 
       OR (home_team_id = 14 AND away_team_id = 12)  
-ORDER BY season 
-DESC LIMIT 5;
+ORDER BY season DESC
+LIMIT 5;
