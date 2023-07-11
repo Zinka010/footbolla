46de1210a89fcaefb71f5383f0287d379a85b65a
Justin Toft <justintoft10@gmail.com>
2023-07-11 12:44:47 -0400

    Added sample SQL and output for getting the head-to-head standings between 2 teams as well as the scores of the last 5 games between them. This is for R11

---
+++ sample-sql/feature-11a-sample-input-1.sql
@@ -0,0 +1,9 @@
+SELECT SUM(team1_win) AS numTeam1Win, SUM(team2_win) AS numTeam2Win, SUM(TIE) AS numTies 
+FROM (
+    SELECT home_team_score > away_team_score AS team1_win, home_team_score < away_team_score AS team2_win, home_team_score = away_team_score AS TIE 
+    FROM footyfiend.MatchesPlayed NATURAL JOIN footyfiend.Matches 
+    WHERE (home_team_id = 12 AND away_team_id = 14) 
+    UNION ALL 
+    SELECT home_team_score > away_team_score AS team1_win, home_team_score < away_team_score AS team2_win, home_team_score = away_team_score AS TIE 
+    FROM footyfiend.MatchesPlayed NATURAL JOIN footyfiend.Matches 
+    WHERE (home_team_id = 14 AND away_team_id = 12)) AS T;
